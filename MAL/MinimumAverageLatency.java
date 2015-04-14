17a9720812140a028e4b480c1c258c4494b53426
Divjot Singh <bogas04@gmail.com>
2015-04-14 19:40:45 +0530

    Working MAL code

--- MAL/MinimumAverageLatency.java
+++ MAL/MinimumAverageLatency.java
@@ -5,22 +5,148 @@ public class MinimumAverageLatency {
     //int nCycles = Integer.parseInt(args[0]);
     //int nStages = Integer.parseInt(args[1]);
     int reservationTable[][] = {
-      {0, 0, 1, 0, 0 ,0, 1},
-      {0, 1, 0, 1, 0 ,0, 1},
-      {0, 0, 0, 0, 0 ,1, 0},
-      {0, 1, 0, 0, 0 ,0, 0},
-      {0, 0, 0, 0, 0 ,0, 0},
-      {0, 0, 0, 1, 1 ,0, 0}
+      {0, 0, 1, 0, 1, 0, 0},
+      {1, 0, 0, 0, 0, 0, 1},
+      {0, 0, 0, 1, 0, 0, 1}
     };
- 
-    // Computing forbidden latencies
-    // For each stage, look for difference between all 1s.
-    //   Naive way :
-    //    Foreach stage
-    //      Foreach 1
-    //        take diff between all subsequent 1s' index and current index, save in a set 
-    //        
-    
-    
+
+    Set<Integer> forbiddenCycles = new HashSet<Integer>();
+
+    for(int i = 0; i < reservationTable.length; i++) {
+      for(int j = 0; j < reservationTable[0].length; j++) {
+        if(reservationTable[i][j] == 1) {
+          for(int k = 0; k < reservationTable[0].length; k++) {
+            if(j != k && reservationTable[i][k] == 1) forbiddenCycles.add(Math.abs(j - k));
+          } 
+        }
+      }
+    }
+    System.out.println("Forbidden Latency Cycle #s : " + forbiddenCycles.toString());
+
+    int initialStateVector = 0;
+    for(int cycle : forbiddenCycles) {
+      initialStateVector += Math.pow(2, cycle - 1);
+    }
+    System.out.println("Initial State Vector : " + 
+        initialStateVector + "d i.e. " + 
+        toBinary(initialStateVector) + "b");  
+
+    LinkedList<State> states = new LinkedList<State>();
+    states.add(new State(initialStateVector, initialStateVector, reservationTable[0].length));
+
+    State parentState = null;
+    while((parentState = getUnvisited(states)) != null) {
+      int currentStateVector = parentState.number;
+
+      System.out.println("\n  Finding states from " + parentState.number + " ( " + toBinary(parentState.number) + " )");
+
+      for(int i = 0; i < reservationTable[0].length; i++) {   
+        boolean isEven = currentStateVector%2 == 0;
+        currentStateVector = currentStateVector >> 1;
+
+        if(isEven) { 
+          currentStateVector = initialStateVector | currentStateVector;
+          System.out.println("   Potential state : " + currentStateVector + " ( " + toBinary(currentStateVector) + " )");
+          boolean found = false;
+          for(State st : states) {  
+            if(st.number == currentStateVector) {
+              found = true;
+              break;
+            }
+          }
+
+          if(!found) {    
+            State temp = new State(currentStateVector, currentStateVector, reservationTable[0].length); 
+            System.out.println("   Confirmed new state");
+            states.add(temp);
+          }
+
+          int indexOfParentState = states.indexOf(parentState);
+          parentState.visited = true;
+
+          if(parentState.cyclesToReach > (i + 1)) {
+            parentState.nextGreedyState = currentStateVector;
+            parentState.cyclesToReach = i + 1; 
+          }     
+          states.set(indexOfParentState, parentState);
+
+        }
+      }
+    }
+    System.out.println("\n\nStates : " + states + "\n");
+
+    // Finding MAL
+
+    State minState = null;
+    int mal = Integer.MAX_VALUE;
+    for(State validState : states) { 
+      State iteratingState = validState;
+
+      System.out.println("\n Cycles from : " + validState.number + " ( " + toBinary(validState.number)+ " )");
+      int cyclesTraversed = 0;
+      int statesTraversed = 0;
+      if(iteratingState.nextGreedyState == validState.number) {
+        cyclesTraversed = validState.cyclesToReach;
+      } else {
+        while(iteratingState.nextGreedyState != validState.number) {
+          System.out.print("  " + iteratingState.number + " ( " + toBinary(iteratingState.number) + " ) [ " + iteratingState.cyclesToReach + " ] -> ");
+          cyclesTraversed += iteratingState.cyclesToReach; 
+          statesTraversed++;
+          iteratingState = getGreedyState(states, iteratingState.nextGreedyState);
+        } 
+      } 
+      System.out.println(iteratingState.number + " ( " + toBinary(iteratingState.number) + " ) [ " + iteratingState.cyclesToReach + " ] -> " + 
+                         validState.number + " ( " + toBinary(validState.number) + " ) ");
+
+      cyclesTraversed += iteratingState.cyclesToReach;
+      statesTraversed++;
+
+      if(mal > cyclesTraversed/statesTraversed) {
+        mal = cyclesTraversed/statesTraversed;
+        minState = validState;
+      }
+    }
+
+    System.out.println("\n MAL from : "+ minState + " , MAL = " + mal);
   }
+  public static State getGreedyState(LinkedList<State> l, int value) {
+    for(State s : l) {
+      if(s.number == value) return s;
+    }
+    return null;
+  }
+  public static State getUnvisited(LinkedList<State> l) { 
+    int visited = 0;
+    for(State s : l) {
+      if(!s.visited) return s;
+    }
+    return null;
+  }
+  public static String toBinary(int num) {
+    StringBuilder sb = new StringBuilder(""); 
+    while(num != 0) {
+      sb.append(num%2);
+      num = num >> 1; 
+    }
+    return sb.reverse().toString();
+  }
+}
+class State {
+  int number;
+  int nextGreedyState;
+  int cyclesToReach;
+  boolean visited = false;
+
+  State(int a, int b, int c) {
+    number = a;
+    nextGreedyState = b;
+    cyclesToReach = c;
+  }
+
+  public String toString() {
+    return "State : " + number + (visited?" V":" NV") + " (" + MinimumAverageLatency.toBinary(number) + ")" +
+      "->" + nextGreedyState + "( " + MinimumAverageLatency.toBinary(nextGreedyState)+ " )" + 
+      "[ " + cyclesToReach+ " ]";
+  }
+
 }
--- MAL/mal.cpp
+++ MAL/mal.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include<vector>
 #include<set>
 #include<cmath>
@@ -9,23 +10,23 @@ struct greedy{
     int to;
     int cycles;
 };
-bool greedy_compare(const greedy *a, const greedy *b) {
-    return (a->from - b->from);
+bool operator<(const greedy& a, const greedy& b) {
+    return (a.from < b.from);
 }
-greedy* find_state (set<greedy, greedy_compare> d,int state_vector) {
-    for(iterator<greedy>::it = d.begin(); it != d.end(); it++) {
-        if(*it.from == state_vector) {
+set<greedy>::iterator find_state (set<greedy> d,int state_vector) {
+    for(set<greedy>::iterator it = d.begin(); it != d.end(); it++) {
+        if(it->from == state_vector) {
             return it;
         }
     }
-    return NULL;
+    return d.end();
 }
 int main() {
 	int n,t;
 	int arr[100][100];
 	vector<int> v;
 	set<int> s;
-	cout << "Enter the number of stages and time required for one instruction to be executed";
+	cout << "Enter the number of stages and cycles in one instruction to be executed : ";
 	cin >> n >> t;
 	for(int i=0;i<n; i++){
 		for(int j=0;j<t;j++){
@@ -37,66 +38,67 @@ int main() {
 		for(int a = 0;a<v.size();a++){
 			for(int b=0;b<v.size();b++){
 				if(a!=b){
-					cout << abs(v[a]-v[b])<<endl;
 					s.insert(abs(v[a]-v[b]));
 				}
 			}
 		}
-			v.clear();
+    v.clear();
 	}
 
 	int initialStateVector = 0;
 	for(set<int>::iterator it = s.begin();it!=s.end();it++){
-		cout << *it;
-		initalStateVector += pow(2,*it-1);
+		initialStateVector += pow(2,*it-1);
 	}
 
-	cout << initalStateVector;
-	greedy initial = {initialStateVector, initalStateVector,t};
+	cout << to_binary(initialStateVector);
+	greedy initial = {initialStateVector, initialStateVector,t};
 
-	set<greedy> d(&greedy_compare>);
+	set<greedy> d;
 	d.insert(initial);
 
-    // for each state
-	for(set<greedy>::iterator it = d.begin();it!=d.end();it++){
-        int current_state = it->from;
+    //// for each state
+	//for(set<greedy>::iterator it = d.begin();it!=d.end();it++){
+        //int current_state = it->from;
 
-        // for each cycle in this state
-        for(int cyclesComputed = 1; cyclesComputed <= t; cyclesComputed++) {
-            //  shift right
-            current_state= current_state>>1;
+        //// for each cycle in this state
+        //for(int cyclesComputed = 1; cyclesComputed <= t; cyclesComputed++) {
+            ////  shift right
+            //current_state= current_state>>1;
 
-            // create new state
-            d.insert({current_state|initialStateVector, initialStateVector, t});
+            //// create new state
+            //greedy l = {current_state|initialStateVector, initialStateVector, t};
+            //d.insert(l);
 
-            // save
-            if(it->cycles > cyclesComputed) {
-                it->to = current_state|initialStateVector;
-                it->cycles = cyclesComputed;
-            }
-        }
-	}
+            //// save
+            //if(it->cycles > cyclesComputed) {
+                    //d.erase(it);
+                    //greedy to_add ={current_state,current_state|initialStateVector,cyclesComputed};
+                    //d.insert(to_add);
+            //}
+        //}
+	//}
 
-	// for each state
-	vector <int> avg_latencies[t];
-	for(set<greedy>::iterator it = d.begin();it!=d.end();it++){
-        greedy current_state = *it;
+	//// for each state
+	//vector <int> avg_latencies[50];
+	//int g=0;
+	//for(set<greedy>::iterator it = d.begin();it!=d.end();it++){
+        //set<greedy>::iterator current_state = it;
 
-        // self loop
-        if(current_state.to == it->from) {
-            avg_latencies.push_back(*it.cycles);
-        } else {
-            int cycles_traversed = 0;
-            int states_traversed = 0;
-            while(current_state.to != *it.from) {
-                cycles_traversed += current_state.cycles;
-                current_state = find_state(d, current_state.to);
-                if(current_state == NULL) { cout << "Unexpected error" ; return 0;}
-                states_traversed++;
-            }
-            avg_latencies.push_back(cycles_traversed/states_traversed);
-        }
-	}
+        //// self loop
+        //if(current_state->to == it->from) {
+            //avg_latencies[g++].push_back(it->cycles);
+        //} else {
+            //int cycles_traversed = 0;
+            //int states_traversed = 0;
+            //while(current_state->to != it->from) {
+                //cycles_traversed += current_state->cycles;
+                //current_state = find_state(d, current_state->to);
+                //if(current_state == d.end()) { cout << "Unexpected error" ; return 0;}
+                //states_traversed++;
+            //}
+            //avg_latencies[g].push_back(cycles_traversed/states_traversed);
+        //}
+	//}
 
-	return 0;
+  return 0;
 }
